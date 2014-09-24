dabd52540251ce89d0adb50d5cc02bb40d1a46c1
mmadlavana@gmail.com <mmadlavana@gmail.com@abc20b46-1a99-9d3c-1a9b-2685fd156449>
2014-09-24 17:40:21 +0000

    ftransc 5.1.3 - fixed distinguishing between ffmpeg and avconv

--- trunk/utils/constants.py
+++ trunk/utils/constants.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
+from subprocess import PIPE, Popen
+
+def _determine_ffmpeg_utility():
+    avutil = None
+    for util in ["avconv", "ffmpeg"]:
+        found = Popen(["which", util], stdout=PIPE).communicate()[0].strip()
+        if found:
+            avutil = util
+            break
+    if avutil is None:
+        raise SystemExit("ffmpeg/avconv not installed")
+    return avutil
+
+
 NO_TAGS             = False
 SILENT              = False  
-VERSION             = '5.1.0'
+VERSION             = '5.1.3'
 LOGFILE             = '/dev/null'
 SUPPORTED_FORMATS   = set(['mp3', 'wma', 'wav', 'ogg', 'flac', 'm4a', 'mpc', 'wv', 'avi'])
 EXTERNAL_FORMATS    = set(["mpc", "wv"])
@@ -23,9 +37,12 @@ EXTERNAL_ENCODER_OUTPUT_OPT = {
     'wavpack'   : '-o',
 }
 
+FFMPEG_AVCONV = _determine_ffmpeg_utility()
+
 DEPENDENCIES = {
     'cdparanoia'        : [],
-    'avconv'            : list(SUPPORTED_FORMATS),
+    FFMPEG_AVCONV       : list(SUPPORTED_FORMATS),
     }
 for audio_format, encoder in EXTERNAL_ENCODERS.iteritems():
     DEPENDENCIES[encoder] = [audio_format]
+
